82561417e680c86ca936b8fbcae5683d22d04f95
acigankov <acigankov@inbox.ru>
2022-12-06 12:14:27 +0300

    Сделал первый экран+шапка+адаптив

--- src/assets/js/mobile-menu.js
+++ src/assets/js/mobile-menu.js
@@ -1,17 +1,18 @@
 const hamburger = document.getElementById('hamburger');
-// const menu = document.querySelector('.mobile-menu');
+ const menu = document.querySelector('.mobile');
 
 //отслеживание клика по бургеру
 hamburger.addEventListener('click', function (event) {
     this.classList.toggle('is-active');
     console.log(this);
 
-    // if(!menu.classList.contains('active')) {
-    //     // document.body.style.overflow = 'hidden';
-    //     menu.classList.add('active');
-    // } else {
-    //     // document.body.style.overflow = 'auto';
-    //     menu.classList.remove('active');
-    // }
+    if(!menu.classList.contains('open')) {
+        document.body.style.overflow = 'hidden';
+        menu.classList.add('open');
+    } else {
+        document.body.style.overflow = 'auto';
+        menu.classList.remove('open');
+    }
     
 });
+
