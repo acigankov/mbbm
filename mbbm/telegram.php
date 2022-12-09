b24b8dec15ee6e7bed18e3bc2e50d0a8ed4093c2
acigankov <acigankov@inbox.ru>
2022-12-09 15:46:13 +0300

    Добавил бота в чат

--- mbbm/telegram.php
+++ mbbm/telegram.php
@@ -2,7 +2,7 @@
 // токен бота
 define('TELEGRAM_TOKEN', '5983589381:AAHOJ48o2oZtaJQe0LRWxLuPYrQa2V1d2pY');
 // ваш внутренний ID
-define('TELEGRAM_CHATID', '135359821');
+define('TELEGRAM_CHATID', '-872112878');
 $message = str_replace("\n", ' ', strip_tags($body));
 $ch = curl_init('https://api.telegram.org/bot' . TELEGRAM_TOKEN . '/sendMessage?chat_id=' . TELEGRAM_CHATID . '&text=' . $message); // URL
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Не возвращать ответ
