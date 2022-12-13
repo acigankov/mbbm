01d68967ba82cc07ef1ca02d5387b8081cdff319
acigankov <acigankov@inbox.ru>
2022-12-13 21:44:53 +0300

    Поправил закрытие скрыто формы

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -6,3 +6,9 @@ const callForm = document.getElementById('intouch-form');
 callBtn.addEventListener('click', function(event) {
     callForm.classList.toggle('open');
 });
+
+window.addEventListener('click', function(event) {
+    if (event.target.className !== 'intouch-form' && !event.target.closest('#intouch-form')  && event.target.className !== 'callback-action' && callForm.classList.contains('open') ) {
+            callForm.classList.remove('open');
+    }
+});
