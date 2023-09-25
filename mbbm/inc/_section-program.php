0f0436d7e34b8ca3a22e139aa1f41704368cfbd3
acigankov@inbox.ru <diiPs56uaY>
2023-09-25 22:43:06 +0300

    Правки по просьбе заказчика

--- mbbm/inc/_section-program.php
+++ mbbm/inc/_section-program.php
@@ -1,7 +1,14 @@
 <section class="program" id="program">
     <div class="container">
         <div class="section-padding section-white">
-            <h1 class="program-heading">Структура курса: </h1>
+            <h1 class="program-heading">учебная программа</h1>
+            <h3 class="program-subheading">основная информация</h3>
+            <div class="program-text">
+                <p>Второй поток программы обучения для женщин предпринимателей "Малый бизнес без мужчин" стартует 24 сентября 2023 года. Обучение проводится в онлайн формате на русском языке.</p>
+                <p>Обучение длится 4 недели и ведётся одновременно двумя Наставниками: Анной Секачёвой, Бизнес-психологом, специализирующимся на работе с женщинами- предпринимателями, и Олегом Стволинским, Бизнес-мастером - действующим предпринимателем и бизнес-тренером, выпустившим уже более 300 учеников на своих программах
+                    по обучению бизнесу.</p>
+                <p>По итогам первого потока выпускницы получили существенные результаты как в части повышения уверенности в себе за счёт работы с Психологом, так и в части систематизации бизнеса и выхода на новые финансовые результаты.</p>
+            </div>
             <div class="list-item">
                 <div class="list-heading">
                     <span class="list-heading__title">Занятие #1</span>
@@ -65,56 +72,9 @@
                     </ul>
                 </div>
             </div><!-- /.list-item -->
-            <div class="list-item">
-                <div class="list-heading">
-                    <span class="list-heading__title">Занятие #5</span>
-                    <span class="list-heading__text">Как сделать бизнес прибыльным?</span>
-                </div>
-                <div class="list-text">
-                    <ul>
-                        <li><span>5.1.</span> Разбор домашнего задания, кейсов и вопросов слушательниц</li>
-                        <li><span>5.2.</span> Как преодолевать страхи и принимать вызовы?</li>
-                        <li><span>5.3.</span> Как преодолевать ограничения и поддерживать мотивацию?</li>
-                        <li><span>5.4.</span> Как построить успешную финансовую модель (юнит-экономику)?</li>
-                        <li><span>5.5.</span> Как оптимизировать затраты?</li>
-                        <li><span>5.6.</span> Как не ошибиться в ценообразовании?</li>
-                        <li><span>5.7.</span> Домашнее задание</li>
-                    </ul>
-                </div>
-            </div><!-- /.list-item -->
-            <div class="list-item">
-                <div class="list-heading">
-                    <span class="list-heading__title">Занятие #6</span>
-                    <span class="list-heading__text">Как выстроить систему?</span>
-                </div>
-                <div class="list-text">
-                    <ul>
-                        <li><span>6.1.</span> Разбор домашнего задания, кейсов и вопросов слушательниц</li>
-                        <li><span>6.2.</span> Как избегать эмоционального выгорания и быть в ресурсе?</li>
-                        <li><span>6.3.</span> Как сфокусироваться на целях?</li>
-                        <li><span>6.4.</span> Как эффективно управлять временем и всё успевать?</li>
-                        <li><span>6.5.</span> Как правильно ставить цели перед сотрудниками и мотивировать команду на результат?</li>
-                        <li><span>6.6.</span> Домашнее задание</li>
-                    </ul>
-                </div>
-            </div><!-- /.list-item -->
-            <div class="list-item">
-                <div class="list-heading">
-                    <span class="list-heading__title">Занятие #7</span>
-                    <span class="list-heading__text">Подведение итогов</span>
-                </div>
-                <div class="list-text">
-                    <ul>
-                        <li><span>7.1.</span> Разбор домашнего задания, кейсов и вопросов слушательниц</li>
-                        <li><span>7.2.</span> Как построить идеальную жизнь?</li>
-                        <li><span>7.3.</span> Как развиваться изо дня в день?</li>
-                        <li><span>7.4.</span> Как наладить семейную жизнь в условиях высокой нагрузки?</li>
-                        <li><span>7.5.</span> Как контролировать бизнес по опережающим показателям?</li>
-                        <li><span>7.6.</span> Как привлекать инвестиции в бизнес?</li>
-                        <li><span>7.7.</span> Домашнее задание</li>
-                    </ul>
-                </div>
-            </div><!-- /.list-item -->
+
+            <img src="/assets/img/mbbmnew.webp" alt="program" class="program-image"><!-- /.program-image -->
+
         </div><!-- /.section-padding -->
     </div><!-- /.container -->
 </section>
